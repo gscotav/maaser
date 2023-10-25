bdfa0eaf844dabf736aed2c8a6bb75db9e53de09
gscotav <gscotav@gmail.com>
2023-10-25 16:48:06 -0400

    styled

--- src/App.tsx
+++ src/App.tsx
@@ -40,45 +40,45 @@ function App() {
   
 
   return (
-    <div>
-      <h1>מעשׂר חשבון</h1>
-      <div>
-        <button onClick={() => setCount((count) => count - money)}>
+    <div className="container">
+    <h1 className="title">מעשר חשבון</h1>
+    <div className="button-container">
+      <button className="action-button" onClick={() => setCount((count) => count - money)}>
         צדקה
-        </button>
-        <> </>
-
-        <input
-          value={money}
-          onChange={(e) => setMoney (e.target.value)}
-          placeholder="Enter your earnings or tzeduka"
-          style={{
-            whiteSpace: 'nowrap',
-            width: '185px',
-          }}
-        />
-        
-        <button onClick={() => setCount((count) => count +  money / 10)}>
-        מעשׂר
-        </button>
-        <p>
-           {feedback}: {accounting.formatMoney(result, '$')}
-        </p>
-        <button onClick={handleSave}>
-          save
-        </button><button
-        onClick={() => {
-          if (window.confirm('Are you sure you want to reset?')) 
-            { setCount(0);
-            handleSave(); }
-        }}
-        >
-          reset
-        </button>
-      </div>
+      </button>
+      <input
+        value={money}
+        onChange={(e) => setMoney(e.target.value)}
+        placeholder="Enter your ernings or Tzedukah"
+        className="money-input"
+      />
+      <button className="action-button" onClick={() => setCount((count) => count + money / 10)}>
+        מעשר
+      </button>
     </div>
-  );
-};
+    <p className="result">
+      {feedback}: {accounting.formatMoney(result, '$')}
+    </p>
+    <div>
+    <button className="save-button" onClick={handleSave}>
+      save
+    </button>
+    <button
+      className="reset-button"
+      onClick={() => {
+        if (window.confirm('Are you sure you want to reset?')) {
+          setCount(0);
+          handleSave();
+        }
+      }}
+    >
+      reset
+    </button>
+    </div>
+  </div>
+);
+}
+;
 
 
 export default App
